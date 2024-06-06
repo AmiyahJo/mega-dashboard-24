9112695639be2d9cd7bf4ea01239fc5e4a95aa03
Amiyah Jones <amiyah.jones@cdpipelinedevshop.com>
2024-06-06 22:30:00 +0000

    quotes added

--- mega-dash/src/components/RandomQuote.jsx
+++ mega-dash/src/components/RandomQuote.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import '/home/codio/workspace/mega-dash/src/components/RandomQuote.css';
 
 const quotes = [
-    {text: "...", author: "..."},
-    {text: "...", author: "..."},
-    {text: "...", author: "..."},
-    {text: "...", author: "..."},
-    {text: "...", author: "..."},
-    {text: "...", author: "..."},
+    {text: "The Secret of getting ahead, is getting started", author: "Mark Twain"},
+    {text: "There is hope, even when your brain tells you there isn't", author: "John Green"},
+    {text: "ALmost everything will work again if you unplug it for a few minutes, including you", author: "Anne Lamott"},
+    {text: "Life is given to live, not survive", author: "Mobammad Natalie"},
+    {text: "The biggest risk a person can take is do nothing", author: "Robert T. Kiyosaki"},
+    {text: "Happiness is not by chance, but by choice", author: "Jim Rohn"},
 ];
 
 const RandomQuote = () => {
@@ -20,7 +20,7 @@ const RandomQuote = () => {
 
     return (
         <div className="random-quote">
-            <div className="quote-text">"{quote.text}</div>
+            <div className="quote-text">"{quote.text}"</div>
             <div className="quote-author">-{quote.author}</div>
             <button onClick={getRandomQuote}>New Quote</button>
         </div>
